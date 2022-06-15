ab954e1254cffaa5a711fc3f873942a463743b2b
Carlos Henrique <carlos.eletronic@hotmai.com>
2022-06-15 17:20:41 -0300

    second component in angular

---
+++ src/Component/cardprofile/cardprofile.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardprofile',
+  templateUrl: './cardprofile.component.html',
+  styleUrls: ['./cardprofile.component.css'],
+})
+export class CardprofileComponent implements OnInit {
+  title: string = '25';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/Component/profile.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardprofileComponent } from './cardprofile/cardprofile.component';
+
+@NgModule({
+  declarations: [CardprofileComponent],
+  imports: [CommonModule],
+  exports: [CardprofileComponent],
+})
+export class ProfileModule {}
